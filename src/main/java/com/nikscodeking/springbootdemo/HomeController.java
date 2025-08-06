853d450c0bcf2bac76620ebe7034e3a94de95aad
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-08-06 23:46:04 +0530

    @RestController
    it will handle the resources and i will send the data response body
    status codes

--- src/main/java/com/nikscodeking/springbootdemo/HomeController.java
+++ src/main/java/com/nikscodeking/springbootdemo/HomeController.java
@@ -1,22 +1,19 @@
 package com.nikscodeking.springbootdemo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RestController
+@RestController // it will handle the resources and i will send the data response body status codes
 public class HomeController {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello Nikhil";
-		
 	}
 }
 
 
 
-
+ 
 
 
 
