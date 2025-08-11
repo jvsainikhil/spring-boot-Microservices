0143106eb0b2c032360401e6001dbc28c9024df4
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-08-11 16:27:11 +0530

    New end point to handle objects

--- src/main/java/com/nikscodeking/springbootdemo/HomeController.java
+++ src/main/java/com/nikscodeking/springbootdemo/HomeController.java
@@ -3,11 +3,26 @@ package com.nikscodeking.springbootdemo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nikscodeking.springbootdemo.model.User;
+
+
+//Creating and object and how we can send the objects back, New end point to handle objects 
+
 @RestController // it will handle the resources and i will send the data response body status codes
 public class HomeController {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello Nikhil";
+	}
+	
+	@RequestMapping("/user")
+	//userDetails - Method ---> We have set the details
+	public User userDetails() {
+		User user =  new User();
+		user.setId("0");
+		user.setName("Nikhil"); 
+		user.setEmailid("jvsainikhil@gmail.com");
+		return user;
 	}
 }
 
