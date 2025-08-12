957c308620bf81d27adce37fe32afe8c5908e8cd
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-08-12 22:55:36 +0530

    @RequestParam
    -> If we have not provided the value then used required = false
    -> If we want to  pass the url with what ever we want we need to change
    -> with name = "email"
    after changing the code this error pops:

--- src/main/java/com/nikscodeking/springbootdemo/HomeController.java
+++ src/main/java/com/nikscodeking/springbootdemo/HomeController.java
@@ -3,8 +3,8 @@ package com.nikscodeking.springbootdemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.nikscodeking.springbootdemo.model.User;
 
 // Creating and object and how we can send the objects back, New end point to handle objects 
@@ -31,5 +31,10 @@ public class HomeController {
 	@GetMapping("/{id}/{id1}/{id2}/{id3}")
 	public String pathvariable(@PathVariable("id") String name, @PathVariable("id1") String name1, @PathVariable("id2") String name2, @PathVariable("id3") String name3) {
 		return "The path variable is: Name is: " + name + " : " + name1 + " : " + name2 + " : " + name3;
+	}
+	
+	@GetMapping("/reqparam")
+	public String requestparam(@RequestParam String name, @RequestParam(name= "email", required = false) String emailId) {
+		return "Your name is: " + name + "\nEmailId: " + emailId;
 	}
 }
