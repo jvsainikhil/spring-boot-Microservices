05dbee41f8671e5e859f017834581a13bca74787
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-08-12 16:09:40 +0530

    @GetMapping("/{id}")
    -> http://localhost:8080/nik

--- src/main/java/com/nikscodeking/springbootdemo/HomeController.java
+++ src/main/java/com/nikscodeking/springbootdemo/HomeController.java
@@ -1,57 +1,35 @@
 package com.nikscodeking.springbootdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nikscodeking.springbootdemo.model.User;
 
-
 //Creating and object and how we can send the objects back, New end point to handle objects 
-
-@RestController // it will handle the resources and i will send the data response body status codes
+// it will handle the resources and i will send the data response body status codes
+@RestController
 public class HomeController {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello Nikhil";
 	}
-	
-	//@RequestMapping(value = "/user", method = RequestMethod.GET) // ---> This get mapping we know but we need to define 
+
+	// @RequestMapping(value = "/user", method = RequestMethod.) // ---> This get
+	// mapping we know but we need to define
 	@GetMapping("/user") // Much easier to see
-	//userDetails - Method ---> We have set the details
+	// userDetails - Method ---> We have set the details
 	public User userDetails() {
-		User user =  new User();
+		User user = new User();
 		user.setId("0");
-		user.setName("Nikhil"); 
+		user.setName("Nikhil");
 		user.setEmailid("jvsainikhil@gmail.com");
 		return user;
 	}
+
+	@GetMapping("/{id}")
+	public String pathvariable(@PathVariable String id) {
+		return "The path variable is: " + id;
+	}
 }
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
